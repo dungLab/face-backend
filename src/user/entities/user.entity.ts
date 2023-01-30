3901a3f3e6ca52a8fa584518cedc29ab168ee56f
YeomJaeSeon <a89541457@gmail.com>
2023-01-30 09:58:41 +0900

    feat: alter table column

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -12,12 +12,23 @@ export class UserEntity {
   @PrimaryGeneratedColumn({ name: 'id', type: 'int', unsigned: true })
   id: number;
 
+  @Column({
+    name: 'nickname',
+    type: 'varchar',
+    comment: '회원가입 시, 랜덤하게 생성',
+  })
+  nickName: string;
+
+  @Column({
+    name: 'type',
+    type: 'varchar',
+    comment: '유저 타입 (kakao, naver, google, apple)',
+  })
+  type: string;
+
   // TODO: refreshToken 고정된 문자열의 길이이면 char(length) 로 변경
   @Column({ name: 'refresh_token', type: 'varchar', nullable: true })
   refreshToken?: string | null;
-
-  @Column({ name: 'nickname', type: 'varchar' })
-  nickName: string;
 
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
