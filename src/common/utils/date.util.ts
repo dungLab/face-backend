aa609197f8a169425fc15776f6093852abaa97c8
YeomJaeSeon <a89541457@gmail.com>
2023-02-04 17:31:18 +0900

    refactor: change luxon date to str format

--- src/common/utils/date.util.ts
+++ src/common/utils/date.util.ts
@@ -4,6 +4,6 @@ export function getCurrentDateFormat(format?: string) {
   return DateTime.fromJSDate(new Date()).toFormat(format);
 }
 
-export function getDateFormat(date: Date, format = 'yyyy-MM-dd hh:mm:ss') {
+export function getDateFormat(date: Date, format = 'yyyy-MM-dd HH:mm:ss') {
   return DateTime.fromJSDate(date).toFormat(format);
 }
