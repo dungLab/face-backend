5be2f013c702fe0b7feae207fbda741a10bc4448
YeomJaeSeon <a89541457@gmail.com>
2023-02-05 13:07:49 +0900

    docs: api response swagger

--- src/photo/dtos/response/photo-response.dto.ts
+++ src/photo/dtos/response/photo-response.dto.ts
@@ -2,13 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class PhotoResponseDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: '사진 id',
+  })
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '사진 s3 url',
+  })
   url: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '사진 설명',
+  })
   description: string;
 
   @ApiProperty({
@@ -16,12 +22,19 @@ export class PhotoResponseDto {
   })
   span: number;
 
+  @ApiProperty({
+    description: '유저 닉네임',
+  })
   @IsString()
   userNickName: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '포토 생성 날짜',
+  })
   createdAt: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '포토의 해시태그들',
+  })
   hashTags: string[];
 }
--- src/user/dtos/response/user-response.dto.ts
+++ src/user/dtos/response/user-response.dto.ts
@@ -1,15 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserReseponseDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: '유저 id',
+  })
   id: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '유저 email',
+  })
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '유저 닉네임',
+  })
   nickName: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: '유저 가입 날짜',
+  })
   createdAt: string;
 }
