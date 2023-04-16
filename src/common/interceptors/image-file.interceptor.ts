f5240443a260f384de203c1e7f7d6a6f9979c68e
YeomJaeSeon <a89541457@gmail.com>
2023-04-16 16:43:39 +0900

    chore: 허용 이미지 mimetype 축약

--- src/common/interceptors/image-file.interceptor.ts
+++ src/common/interceptors/image-file.interceptor.ts
@@ -10,20 +10,7 @@ export const imageFileFilter = (
   file: Express.Multer.File,
   callback: any,
 ) => {
-  if (
-    ![
-      'image/gif',
-      'image/png',
-      'image/jpeg',
-      'image/jpg',
-      'image/pipeg',
-      'image/bmp',
-      'image/webp',
-      'image/tiff',
-      'image/svg+xml',
-      'image/heic',
-    ].includes(file.mimetype)
-  ) {
+  if (!['image/png', 'image/jpeg', 'image/jpg'].includes(file.mimetype)) {
     callback(
       new ErrorResponse(HttpStatus.BAD_REQUEST, {
         message: `허용되지 않은 minetype입니다. ${file.mimetype}`,
