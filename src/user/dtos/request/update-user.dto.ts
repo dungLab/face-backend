941fffc59716bf8c3a83aedce6acf568f6d4589d
YeomJaeSeon <a89541457@gmail.com>
2023-05-07 15:28:44 +0900

    chore: UpdateUserDto Optional

--- src/user/dtos/request/update-user.dto.ts
+++ src/user/dtos/request/update-user.dto.ts
@@ -1,31 +1,41 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString, Length, ValidateIf } from 'class-validator';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+  ValidateIf,
+} from 'class-validator';
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsString()
   @ApiProperty({ description: '닉네임' })
   @Length(1, 255)
-  nickName: string;
+  nickName?: string;
 
+  @IsOptional()
   @ValidateIf((object, value) => value !== null)
   @IsString()
   @ApiProperty({ description: '간단한 자기소개' })
   @Length(1, 512)
-  introduction: string | null;
+  introduction?: string | null;
 
+  @IsOptional()
   @ValidateIf((object, value) => value !== null)
   @IsString()
   @ApiProperty({
     description: '자기소개 링크 주소(ex) 인스타 주소, 페북, 노션..)',
   })
   @Length(1, 512)
-  link: string | null;
+  link?: string | null;
 
+  @IsOptional()
   @ValidateIf((object, value) => value !== null)
   @IsNumber()
   @ApiProperty({
     description:
       'file id - 프로필 이미지 제거하고싶으면 fileId에 null보내시면 됩니다.',
   })
-  fileId: number | null;
+  fileId?: number | null;
 }
