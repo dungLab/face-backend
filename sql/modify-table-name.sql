56c9c64356353392d35139a408034142504d1156
YeomJaeSeon <a89541457@gmail.com>
2023-05-15 18:50:31 +0900

    fix: rename ddl

--- sql/modify-table-name.sql
+++ sql/modify-table-name.sql
@@ -1,17 +1,17 @@
-RENAME face.FILE TO face.files;
+RENAME TABLE face.FILE TO face.files;
 
-RENAME face.FILE_META TO face.file_metas;
+RENAME TABLE face.FILE_META TO face.file_metas;
 
-RENAME face.HASHTAG TO face.hashtags;
+RENAME TABLE face.HASHTAG TO face.hashtags;
 
-RENAME face.`LOG-EVALUATION` TO face.log_evaluations;
+RENAME TABLE face.`LOG-EVALUATION` TO face.log_evaluations;
 
-RENAME face.USER to face.users;
+RENAME TABLE face.USER to face.users;
 
-RENAME face.PHOTO to face.photos;
+RENAME TABLE face.PHOTO to face.photos;
 
-RENAME face.EVALUATION to face.evaluations;
+RENAME TABLE face.EVALUATION to face.evaluations;
 
-RENAME face.PHOTO-HASHTAG to face.photo_hashtags;
+RENAME TABLE face.`PHOTO-HASHTAG` to face.photo_hashtags;
 
-RENAME face.PROFILE to face.profiles;
+RENAME TABLE face.PROFILE to face.profiles;
